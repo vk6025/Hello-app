d706ef8ec74cb0e8a92736b965f2be622957bd7c
vk6025 <vk6025@srmist.edu.in>
2026-03-26 10:24:20 +0530

    Implement HelloApp to greet users

---
+++ uc_5.java
@@ -0,0 +1,18 @@
+public class HelloApp {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            StringBuilder nameBuilder = new StringBuilder();
+            boolean first = true;
+            for (String name : args) {
+                if (!first) {
+                    nameBuilder.append(", ");
+                }
+                nameBuilder.append(name);
+                first = false;
+            }
+            System.out.println("Hello, " + nameBuilder.toString() + "!");
+        }
+    }
+}
